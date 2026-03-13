953a3077a0b86e6795a42a27440040b7f5afc0a4
Sahir Masoodi <sahir.masoodi@gmail.com>
2026-03-13 23:09:12 +0530

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
-const url ="mongodb+srv://sahirmasoodi:sahir.masoodi@cluster0.ud2xzea.mongodb.net/careplus";
+// const url ="mongodb+srv://sahirmasoodi:sahir.masoodi@cluster0.ud2xzea.mongodb.net/careplus";
 
 const connectDB = async () => {
-  await mongoose.connect(url);
+  await mongoose.connect(process.env.MONGO_URL);
 };
 
-module.exports = {connectDB}
+module.exports = { connectDB };
